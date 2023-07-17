558c9f8fb25d4f2d7211380a05856db7c59317e9
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-07-17 22:08:42 +0200

    Add config store

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+  reducer: {},
+});
+
+export default store;
