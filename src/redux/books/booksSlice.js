05437d92a75452b4db14b336c205572b0773c5b8
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-07-17 22:33:10 +0200

    add addbook and removebook reducers

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [];
+
+const bookSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    addBook: (state, action) => [...state, action.payload],
+    removeBook: (state, action) => {
+      state.filter((item) => item.id !== action.payload);
+    },
+  },
+});
+
+export default bookSlice.reducers;
