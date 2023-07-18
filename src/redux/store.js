b8dbf353049f6b063ab38d62a78f769716d27db6
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-07-18 08:18:45 +0200

    Add reducers in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { configureStore } from '@reduxjs/toolkit';
+import bookReducer from './books/booksSlice';
+import categoriesReducer from './categories/categoriesSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    books: bookReducer,
+    categories: categoriesReducer,
+  },
 });
 
 export default store;
